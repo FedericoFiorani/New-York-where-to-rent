f0627b53a7d8649c1802bdca129ba487019e44b4
martontomka11 <89907077+martontomka11@users.noreply.github.com>
2021-10-05 13:52:00 +0200

    Add files via upload

---
+++ data/Data_download.R
@@ -0,0 +1,25 @@
+install.packages('googledrive')
+install.packages('readr')
+
+#libraries
+library(googledrive)
+library(readr)
+
+#data download
+files = list("https://drive.google.com/file/d/1yUQ0m5VH6IR-2EGAGWvmKLqnpVmhnNXH/view?usp=sharing",
+             "https://drive.google.com/file/d/1kPUe5l67rsq2VkyHKj8_urST52lnteag/view?usp=sharing")
+
+for (f in files) {
+  cat(paste0('Downloading file: ', f, '...\n'))
+  
+  drive_download(
+    file = f,
+    overwrite = TRUE)
+}
+
+#Viewing data in R
+ny_df <- read_csv("listings.csv")
+View(listings)
+
+calendar <- read_csv("calendar.csv")
+View(calendar)
